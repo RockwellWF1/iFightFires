c4bc912a0ca8073fc43bbf16588358f71c7562f2
Willard Rockwell <RockwellWF1@gcc.edu>
2013-02-08 14:51:54 -0500

    Commits Project

---
+++ iFightFires/iFightFires/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iFightFires
+//
+//  Created by Willard Rockwell on 2/8/13.
+//  Copyright (c) 2013 GCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iFightFires/iFightFires/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iFightFires
+//
+//  Created by Willard Rockwell on 2/8/13.
+//  Copyright (c) 2013 GCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
